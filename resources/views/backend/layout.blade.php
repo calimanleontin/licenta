9cfeed920b8badf02b403214687ac7262ffbda26
calimanleontin <calimanleontin@gmail.com>
2016-06-01 01:00:40 +0300

    change backend css

--- resources/views/backend/layout.blade.php
+++ resources/views/backend/layout.blade.php
@@ -2,19 +2,20 @@
 
 
 @section('title')
+
     <div class="col-md-3 menu text-center">
          Backend
     </div>
 @endsection
 @section('title1')
-    @yield('title1')
+    <div class="text-center">@yield('title1')</div>
 @endsection
 
     <link href="{{ asset('/css/backend.css') }}" rel="stylesheet">
 @section('content')
     <div class="col-md-12 backend">
        <div class="">
-           <div class="col-md-3 menu">
+           <div class="col-md-3 col-sm-6 col-xs-12 menu">
                <a href="/backend/products"><button class="btn btn-default text-center button-large">Products</button></a>
                <a href="/backend/categories"><button class="btn btn-default text-center button-large">Categories</button></a>
                <a href="/backend/classes"><button class="btn btn-default text-center button-large">Classes</button></a>
--- resources/views/championship/index.blade.php
+++ resources/views/championship/index.blade.php
@@ -1,20 +1,39 @@
 @extends('app-game')
 @section('content')
 
-    <div class="panel panel-danger">
+    <div class="panel gray absolute">
 
-        <div class="panel-heading">
-            Active Championships
-        </div>
-        <div class="panel-danger">
-            <div class="prize col-md-12 col-sm-12 col-xs-12">
-                @foreach($championships as $championship)
-
-
-                @endforeach
-            </div>
-
+        <div class="panel-heading text-center">
+            <h1>Active Championships</h1>
         </div>
     </div>
+        <div class="">
+            <div class="prize col-md-12 col-sm-12 col-xs-12">
+                @foreach($championships as $championship)
+                    <div class="championship">
+                        <div class="col-md-12">
+                            <div class="text-center text-capitalize col-md-12">
+                                <div class="championship-title">
+                                    <h2>{{ $championship->name }}</h2>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="col-md-12 championship-subtitle">
+                                    <div class="col-md-4">
+                                        <strong>Required level: </strong> {{ $championship->level_required }}
+                                    </div>
+                                    <div class="col-md-4">
+                                        <strong>Reward: </strong> ${{ $championship->reward }}
+                                    </div>
+                                    <div class="col-md-4">
+                                        <strong>Experience: </strong> {{ $championship->max_experience }} exp
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
 
 @endsection
