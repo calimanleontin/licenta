a6312f07b47fcb69206713dad93b762f109d0c7e
calimanleontin <calimanleontin@gmail.com>
2016-06-04 16:48:17 +0300

    view

--- resources/views/home-game.blade.php
+++ resources/views/home-game.blade.php
@@ -7,14 +7,18 @@
 
 @section('content')
     <div class="col-md-12">
-        <div class="col-md-3">
-            @if($hero->sex == 'masc')
-                <img src="../images/champions/riven.jpg" class="hero-avatar">
+        <div class="hero">
+            <div class= hero-avatar">
+                @if($hero->sex == 'masc')
+                    <img src="../images/champions/darius.jpg" class="hero-avatar">
                 @else
-                <img src="../images/champions/darius.jpg" class="hero-avatar">
+                    <img src="../images/champions/riven.jpg" class="hero-avatar">
 
-            @endif
+                @endif
+            </div>
+            <div class="hero-stats">
+
+            </div>
         </div>
     </div>
-
 @endsection
