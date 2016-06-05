d5207dbe36367c1a4c09ca60f8e9164069cbffe3
calimanleontin <calimanleontin@gmail.com>
2016-06-05 17:50:57 +0300

    create work and outside places

--- database/migrations/2016_05_22_103768_create_outside_places_table.php
+++ database/migrations/2016_05_22_103768_create_outside_places_table.php
@@ -20,8 +20,64 @@ class CreateOutsidePlacesTable extends Migration {
 			$table->integer('time_spent')->unsigned();
 			$table->integer('experience');
 			$table->integer('reward');
+			$table->date('started_on');
+			$table->date('ends_on');
 			$table->timestamps();
 		});
+
+		$outside = new \App\ExternalPlaces();
+		$outside->name = 'Forrest';
+		$outside->level_required = 10;
+		$outside->time_spent = 2;
+		$outside->started_on = \Carbon\Carbon::now();
+		$outside->ends_on = \Carbon\Carbon::now()->addHours($outside->time_spent);
+		$outside->experience = 80;
+		$outside->reward = 100;
+		$outside->save();
+
+
+		$outside = new \App\ExternalPlaces();
+		$outside->name = 'Sea';
+		$outside->level_required = 20;
+		$outside->time_spent = 7;
+		$outside->experience = 200;
+		$outside->reward = 1000;
+		$outside->started_on = \Carbon\Carbon::now();
+		$outside->ends_on = \Carbon\Carbon::now()->addHours($outside->time_spent);
+		$outside->save();
+
+
+		$outside = new \App\ExternalPlaces();
+		$outside->name = 'Mountain';
+		$outside->level_required = 30;
+		$outside->time_spent = 20;
+		$outside->experience = 800;
+		$outside->started_on = \Carbon\Carbon::now();
+		$outside->ends_on = \Carbon\Carbon::now()->addHours($outside->time_spent);
+		$outside->reward = 2000;
+		$outside->save();
+
+
+		$outside = new \App\ExternalPlaces();
+		$outside->name = 'Volcano';
+		$outside->level_required = 40;
+		$outside->time_spent = 30;
+		$outside->experience = 1600;
+		$outside->started_on = \Carbon\Carbon::now();
+		$outside->ends_on = \Carbon\Carbon::now()->addHours($outside->time_spent);
+		$outside->reward = 5000;
+		$outside->save();
+
+
+		$outside = new \App\ExternalPlaces();
+		$outside->name = 'Hell';
+		$outside->level_required = 50;
+		$outside->time_spent = 48;
+		$outside->experience = 8000;
+		$outside->started_on = \Carbon\Carbon::now();
+		$outside->ends_on = \Carbon\Carbon::now()->addHours($outside->time_spent);
+		$outside->reward = 11000;
+		$outside->save();
 	}
 
 	/**
--- database/migrations/2016_05_22_103772_create_works_table.php
+++ database/migrations/2016_05_22_103772_create_works_table.php
@@ -18,10 +18,38 @@ class CreateWorksTable extends Migration {
 			$table->string('name');
 			$table->integer('time_spent');
 			$table->integer('experience');
+			$table->date('started_on');
+			$table->date('ends_on');
 			$table->integer('reward');
-
 			$table->timestamps();
 		});
+
+		$work = new \App\Work();
+		$work->name = 'Home';
+		$work->time_spent = 2;
+		$work->started_on = \Carbon\Carbon::now();
+		$work->ends_on = \Carbon\Carbon::now()->addHours($work->time_spent);
+		$work->experience = 10;
+		$work->reward = 500;
+		$work->save();
+
+		$work = new \App\Work();
+		$work->name = 'Sea';
+		$work->time_spent = 8;
+		$work->started_on = \Carbon\Carbon::now();
+		$work->ends_on = \Carbon\Carbon::now()->addHours($work->time_spent);
+		$work->experience = 80;
+		$work->reward = 5000;
+		$work->save();
+
+		$work = new \App\Work();
+		$work->name = 'Jungle';
+		$work->time_spent = 12;
+		$work->started_on = \Carbon\Carbon::now();
+		$work->ends_on = \Carbon\Carbon::now()->addHours($work->time_spent);
+		$work->experience = 120;
+		$work->reward = 10000;
+		$work->save();
 	}
 
 	/**
